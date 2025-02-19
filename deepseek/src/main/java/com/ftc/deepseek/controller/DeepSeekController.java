05f0a455a060a136c90dae665a498ec18c5647cc
fengtiecheng <17615007230@163.com>
2025-02-19 16:37:09 +0800

    feat(ALL): 项目初始化
    1.项目初始化

--- deepseek/src/main/java/com/ftc/deepseek/controller/DeepSeekController.java
+++ deepseek/src/main/java/com/ftc/deepseek/controller/DeepSeekController.java
@@ -3,7 +3,9 @@ package com.ftc.deepseek.controller;
 import io.github.pigmesh.ai.deepseek.core.DeepSeekClient;
 import io.github.pigmesh.ai.deepseek.core.chat.ChatCompletionResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,10 @@ import reactor.core.publisher.Flux;
  * @date: 2025-02-19 15:47:06
  * @describe: DeepSeekController
  */
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/deepseek/")
 public class DeepSeekController {
 
@@ -23,6 +27,9 @@ public class DeepSeekController {
 
     @GetMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ChatCompletionResponse> chat(String prompt) {
+        log.info("[deepseek] 入参词条:[{}]", prompt);
+
+        //1.流式处理返回
         return deepSeekClient.chatFluxCompletion(prompt);
     }
 }
