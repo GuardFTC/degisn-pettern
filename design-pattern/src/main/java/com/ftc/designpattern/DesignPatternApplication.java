1b882613a1a42cc162b116de02b715488a7249b2
fengtiecheng <17615007230@163.com>
2025-03-17 10:33:26 +0800

    feat(ALL): 项目初始化
    1.项目初始化
    2.编写单例模式相关代码

--- design-pattern/src/main/java/com/ftc/designpattern/DesignPatternApplication.java
+++ design-pattern/src/main/java/com/ftc/designpattern/DesignPatternApplication.java
@@ -1,16 +1,13 @@
-package com.ftc.deepseek;
+package com.ftc.designpattern;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author 17615007230@163.com
- */
 @SpringBootApplication
-public class DeepseekApplication {
+public class DesignPatternApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeepseekApplication.class, args);
+        SpringApplication.run(DesignPatternApplication.class, args);
     }
 
 }
---
+++ design-pattern/src/main/java/com/ftc/designpattern/creater/Singleton.java
@@ -0,0 +1,48 @@
+package com.ftc.designpattern.creater;
+
+/**
+ * @author: 冯铁城 [17615007230@163.com]
+ * @date: 2025-03-17 10:20:06
+ * @describe: 单例模式实例代码
+ */
+public class Singleton {
+
+    /**
+     * 静态变量私有化
+     */
+    private static volatile Singleton SINGLETON;
+
+    /**
+     * 构造函数私有化
+     */
+    private Singleton() {
+    }
+
+//    /**
+//     * 获取单例对象
+//     *
+//     * @return 单例对象
+//     */
+//    public static Singleton getSingleton() {
+//        if (singleton == null) {
+//            singleton = new Singleton();
+//        }
+//        return singleton;
+//    }
+
+    /**
+     * 获取单例对象
+     *
+     * @return 单例对象
+     */
+    public static Singleton getSingleton() {
+        if (SINGLETON == null) {
+            synchronized (Singleton.class) {
+                if (SINGLETON == null) {
+                    SINGLETON = new Singleton();
+                }
+            }
+        }
+        return SINGLETON;
+    }
+}
